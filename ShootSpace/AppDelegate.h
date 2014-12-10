4d4a20436f46fe94af6572cf902c6afc938689c9
garyguo2011 <xguo@berkeley.edu>
2014-12-09 16:34:40 -0800

    Initial Commit

---
+++ ShootSpace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShootSpace
+//
+//  Created by Gary Guo-I on 12/9/14.
+//  Copyright (c) 2014 XinranGuo-XuHe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShootSpace/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  ShootSpace
+//
+
+//  Copyright (c) 2014 XinranGuo-XuHe. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
